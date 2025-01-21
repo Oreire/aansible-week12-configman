e19cb05087ee7267f4b78d425280a19d2a110f7a
Ayomide Ajayi <lancerajayi@yahoo.com>
2025-01-21 11:25:37 +0000

    Ansible CM Testing 15

--- main.tf
+++ main.tf
@@ -108,5 +108,21 @@ resource "aws_instance" "ubuntu_node" {
   }
 } */
 
+resource "null_resource" "ansible_inventory" {
+  depends_on = [
+    aws_instance.amazon_linux_node,
+    aws_instance.ubuntu_node,
+  ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "[amazon_linux_nodes]" > inventory.ini
+      terraform output -json amazon_linux_node_ips | jq -r '.[] | . + " ansible_ssh_user=ec2-user"' >> inventory.ini
+      echo "\n[ubuntu_nodes]" >> inventory.ini
+      terraform output -json ubuntu_node_ips | jq -r '.[] | . + " ansible_ssh_user=ubuntu"' >> inventory.ini
+    EOT
+  }
+}
+
 
 
