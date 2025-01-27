934255ea3d8c328781cde67359b23f5b7e150176
Ayomide Ajayi <lancerajayi@yahoo.com>
2025-01-27 18:16:08 +0000

    Provisioned Nodes Testing Workflows

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
 
 
 output "ansible_control_node_ip" {
-  value = aws_instance.ansible_control_node.public_ip
+  value = aws_instance.ansible_control_node.private_ip
 }
 
 output "amazon_linux_node_ips" {
-  value = [for instance in aws_instance.amazon_linux_node : instance.public_ip]
+  value = [for instance in aws_instance.amazon_linux_node : instance.private_ip]
 }
 
 output "ubuntu_node_ips" {
-  value = [for instance in aws_instance.ubuntu_node : instance.public_ip]
+  value = [for instance in aws_instance.ubuntu_node : instance.private_ip]
 }
